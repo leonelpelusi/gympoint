cfedeaef1944e2779518d96d6b740368902cb558
Leonel <leonelpelusi@gmail.com>
2020-01-06 16:05:21 -0300

    subindo aplicação

---
+++ src/app/models/User.js
@@ -0,0 +1,20 @@
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        token: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default User;
